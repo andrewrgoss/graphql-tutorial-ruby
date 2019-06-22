889871e97a1473e48fb4ce0074897a770b708daf
andrewrgoss <andrewrgoss@gmail.com>
2019-06-22 17:35:08 -0400

    adjust line breaks for visualization

--- app/graphql/mutations/sign_in_user.rb
+++ app/graphql/mutations/sign_in_user.rb
@@ -18,7 +18,8 @@ module Mutations
       return unless user.authenticate(email[:password])
 
       # use Ruby on Rails - ActiveSupport::MessageEncryptor, to build a token
-      crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
+      crypt = ActiveSupport::MessageEncryptor.new(Rails
+         .application.credentials.secret_key_base.to_s.byteslice(0..31))
       token = crypt.encrypt_and_sign("user-id:#{ user.id }")
 
       context[:session][:token] = token
